5a147b015008bdce1cb40c4dd35a907a1326ba6e
kulombok <wh.code.journey@gmail.com>
2022-08-10 19:31:06 +0700

    mini aplikasi

--- 220808classroom.hs
+++ 220808classroom.hs
@@ -87,7 +87,7 @@ getColor (Person _ _ x) = x
 
 whatIsNameField :: Person -> String
 whatIsNameField p@(Person x _ _) = "The name field of (" ++show p++ ") is "++ x
--- whatIsNameField john -"The name field of (Person \"Jhony\" 15 Red) is Jhony"> expected 
+-- whatIsNameField john -> expected  "The name field of (Person \"Jhony\" 15 Red) is Jhony"
 
 -- Mini Application (MENU PIZZA)
 data Menu = Menu { menuname :: String, menuprice :: Int} deriving Show
@@ -102,27 +102,87 @@ addOrder2 :: String -> Int -> Int -> Menu
 addOrder2 namamenu jumlah hargasatuan = Menu {menuname = namamenu, menuprice = jumlah * hargasatuan}
 --  addOrder2 "Pizza" 2 2000 -> expected Menu {menuname = "Pizza", menuprice = 4000}
 
+orderBook :: [Menu] -> String -> Int -> Int -> [Menu]
+orderBook oldOrder namamenu jumlah hargasatuan = do 
+                                       let orderBaru = Menu {menuname = namamenu, menuprice = jumlah * hargasatuan} 
+                                       let newOrderList =  oldOrder ++ [orderBaru]
+                                       newOrderList
+
+        
 -- Interactive input by User
 main :: IO()
-main = do
-        putStrLn "Silahkan masukan pesanan ? "
-        namamenu <- getLine
-        putStrLn "\nJumlah yang ingin dibeli ? "
-        jumlahmenu <- getLine
-        putStrLn "\nHarga satuan ? "
-        hargasatuan <- getLine
-        let pesanan = addOrder namamenu (read jumlahmenu) (read hargasatuan) -- read convert string to integer
-        putStrLn "\n==========   Pesanan Anda adalah   ===========\n"
-        print pesanan
-        putStrLn "\n========== Pesanan sedang diproses ===========\n"
-        main
+main = pesan []
+
+keluar :: [Menu] -> IO()
+keluar listOrder = do
+        putStrLn "\n========== List pesanan Anda: =========="
+        print listOrder
+        putStrLn "\n==========    Terima kasih    =========="
+
+pesan :: [Menu] -> IO()
+pesan isipesanan = do
+        putStrLn "(i) Input Menu"
+        putStrLn "(q) Quit/Keluar"
+        pilihan <- getLine
+
+        case pilihan of
+                "i" -> do
+                        putStrLn "\nSilahkan masukan pesanan ? "
+                        namamenu <- getLine
+                        putStrLn "\nJumlah yang ingin dibeli ? "
+                        jumlahmenu <- getLine
+                        putStrLn "\nHarga satuan ? "
+                        hargasatuan <- getLine
+                        let pesanan = orderBook isipesanan namamenu (read jumlahmenu) (read hargasatuan) -- read convert string to integer
+                        putStrLn "\n==========   Pesanan Anda adalah   ===========\n"
+                        print pesanan
+                        putStrLn "\n========== Pesanan sedang diproses ===========\n"
+                        pesan pesanan
+                _ -> keluar isipesanan
 -- main -> expected
+-- (i) Input Menu
+-- (q) Quit/Keluar
+-- i
+-- 
 -- Silahkan masukan pesanan ? 
--- Pizza
--- Jumlah yang ingin dibeli ? 
--- 3
--- Harga satuan ? 
+-- pizza
+-- 
+-- Jumlah yang ingin dibeli ?
+-- 2
+-- 
+-- Harga satuan ?
 -- 100000
---  ========== Pesanan Anda adalah ===========
--- Menu {menuname = "Pizza", menuprice = 300000}
---  ========== Pesanan sedang diproses ===========
+-- 
+-- ==========   Pesanan Anda adalah   ===========
+-- 
+-- [Menu {menuname = "pizza", menuprice = 200000}]
+-- 
+-- ========== Pesanan sedang diproses ===========
+-- 
+-- (i) Input Menu
+-- (q) Quit/Keluar
+-- i
+-- 
+-- Silahkan masukan pesanan ? 
+-- hamburger
+-- 
+-- Jumlah yang ingin dibeli ?
+-- 1
+-- 
+-- Harga satuan ?
+-- 50000
+-- 
+-- ==========   Pesanan Anda adalah   ===========
+-- 
+-- [Menu {menuname = "pizza", menuprice = 200000},Menu {menuname = "humberger", menuprice = 50000}]
+-- 
+-- ========== Pesanan sedang diproses ===========
+-- 
+-- (i) Input Menu
+-- (q) Quit/Keluar
+-- q
+-- 
+-- ========== List pesanan Anda: ==========
+-- [Menu {menuname = "pizza", menuprice = 200000},Menu {menuname = "humberger", menuprice = 50000}]
+-- 
+-- ==========    Terima kasih    ==========
